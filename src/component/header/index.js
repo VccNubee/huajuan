89bceb892c01a870f557956a72f63071ed5803b5
VccNubee <yo1398330798@qq.com>
2019-02-25 15:07:55 +0800

    2

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,11 +1,16 @@
-import {NavLink} from 'react-router-dom'
+import {NavLink, Link} from 'react-router-dom'
 import React,{Component} from 'react'
+import ccc from './index.module.scss'
 
 
 class Header extends Component{
     render() {
-     return <div>
-        Header
+     return <div id='Header'>
+        <div className={ccc.top}>
+            <div>
+                <Link to='/home' className={ccc.a1}><span class="iconfont icon-1"></span>首页</Link>
+            </div>
+        </div>
      </div>
     }
 }
