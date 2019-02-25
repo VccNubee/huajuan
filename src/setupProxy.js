dc02f7ca7d0597211ed5b528efae2a0b5d4ec2d5
liyj <1983918044@qq.com>
2019-02-25 14:06:31 +0800

    反向代理

---
+++ src/setupProxy.js
@@ -0,0 +1,9 @@
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(proxy('/pc', {
+  	target: 'https://www.huajuanmall.com',
+  	changeOrigin:true
+  }));
+
+};
