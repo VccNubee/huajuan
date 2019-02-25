f060c09b5a17f5ba136df9446b139d00ef929d30
VccNubee <yo1398330798@qq.com>
2019-02-25 13:45:58 +0800

    2

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import App from '../App';
+import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Home from '../views/home'
+import List from '../views/list'
+import Gdetail from '../views/Gdetail'
+import Vdetail from '../views/Vdetail'
+import Hot from '../views/hot'
+import Search from '../views/search'
+import ChargeTutoria from '../views/chargeTutoria'
+
+var router = (
+    <Router>
+        <App>
+            <Switch>
+                <Route path='/home/:id' component={Home}></Route>
+                <Route path='/list/:id' component={List}></Route>
+                <Route path='/gdetail/:id' component={Gdetail}></Route>
+                <Route path='/vdetail/:id' component={Vdetail}></Route>
+                <Route path='/hongren/:id' component={Hot}></Route>
+                <Route path='/search' component={Search}></Route>
+                <Route path='/chargeTutoria' component={ChargeTutoria}></Route>
+                <Redirect from='/' to='/home'></Redirect>
+            </Switch>
+        </App>
+    </Router>
+)
+export default router
