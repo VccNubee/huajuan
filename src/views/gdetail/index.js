4e57317eb67b44f6b3d345d4d69521d73f10b079
liyj <1983918044@qq.com>
2019-02-25 14:47:40 +0800

    iconfont

--- src/views/gdetail/index.js
+++ src/views/gdetail/index.js
@@ -1,22 +1,20 @@
 import React,{Component} from 'react'
 
-class Gdetail extends Component{
+class gDetail extends Component{
 	constructor(props) {
 	  super(props);
-	
+
 	  this.state = {};
 	}
 
-	componentDidMount(){
-		// console.log(this.props.location.state.myid)
-	}
 
 	render(){
-		return <div>
-			Gdetail
-		</div>
+		return (
+						<div>
+								Gdetail
+						</div>
+				)
 	}
-
 }
 
-export default Gdetail
+export default gDetail
