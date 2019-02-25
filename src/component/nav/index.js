92c59e998a7e4f4323cb74f9e35337da8b932a6a
VccNubee <yo1398330798@qq.com>
2019-02-25 20:23:55 +0800

    2

--- src/component/nav/index.js
+++ src/component/nav/index.js
@@ -1,12 +1,53 @@
 import {NavLink} from 'react-router-dom'
 import React,{Component} from 'react'
+import ccc from './index.module.scss'
+import axios from 'axios'
 
 
 class Nav extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            pList:[]
+        }
+    }
+    componentDidMount(){
+        axios('/pc/pcIndex/class').then((res)=>{
+            this.setState({
+                pList:res.data.goodsClass
+            })
+            console.log(this.state.pList)
+        })
+    }
     render() {
-     return <div id='nav'>
-     nav
+     return <div id={ccc.nav}>
+        <ul>
+            {
+                this.state.pList.map(
+                    (item)=><li key={item.gc_id}>
+                        <NavLink to={`/list/${item.gc_id}`} activeClassName={ccc.active}>
+                            {item.pc_icon?
+                            <img src={item.pc_icon} alt=""/>:
+                            <i></i>}
+                            <span>{item.gc_name}</span>
+                        </NavLink>
+                        <ul ref='myel'>
+                        {
+                            item.children.map(
+                                (item2)=>
+                                <li key={item2.gc_id}><NavLink to={`/list/${item2.gc_id}`}>{item2.gc_name}</NavLink></li>
+                            )
+                        }
+                        </ul>
+                    </li>
+                    
+                )
+            }
+            <li><NavLink to='/chargeTutoria' activeClassName={ccc.active}><i></i>化妆教学</NavLink></li>
+            
+        </ul>
      </div>
     }
 }
+
 export default Nav
--- src/views/list/index.js
+++ src/views/list/index.js
@@ -1,19 +1,71 @@
 import React,{Component} from 'react'
-
+import ccc from './index.module.scss'
+import axios from 'axios'
+import {NavLink,Link} from 'react-router-dom'
 class List extends Component{
 	constructor(props) {
 	  super(props);
 	
-	  this.state = {};
+	  this.state = {
+		  pList:[],
+		  aList:[]
+	  };
 	}
-
 	componentDidMount(){
-		// console.log(this.props.location.state.myid)
+		axios(`/pc/goods/gcGoods?gc_id=${this.props.match.params.id}&limit=15&offset=0`).then(
+			(res)=>{
+				this.setState({
+					pList:res.data.goods_info
+				})
+				console.log(this.state.pList)
+			}
+		)	
+		
+	}
+	componentWillReceiveProps(nextprops){
+		// axios('/pc/pcIndex/class').then((res)=>{
+		// 	this.setState({
+		// 		aList:res.data.goodsClass
+		// 	})
+		// 	console.log(nextprops)
+		// })
+		axios(`/pc/goods/gcGoods?gc_id=${nextprops.match.params.id}&limit=15&offset=0`).then(
+			(res)=>{
+				this.setState({
+					pList:res.data.goods_info
+				})
+				console.log(this.state.pList)
+			}
+		)
 	}
 
 	render(){
-		return <div>
-			List
+		return <div id={ccc.List}>
+			<ul>
+				<li>品类</li>
+				{
+					this.state.aList.map(
+						(item)=>
+						<li key={item.gc_id}><NavLink activeClassName={ccc.active} to={`/list/${item.gc_id}`}>{item.gc_name}</NavLink></li>
+					)
+				}
+			</ul>
+			<ul className={ccc.dataList}>
+				{
+					this.state.pList.map(
+						(item)=><li key={item.goods_id}>
+							<Link to={`/detail/${item.goods_id}`}>
+								<div>
+									<img src={item.goods_image} alt=""/>
+								</div>
+								<p className={ccc.p1}>{item.goods_desc}</p>
+								<p className={ccc.p2}>{item.goods_name}</p>
+								<p className={ccc.p3}><span className={ccc.s1}>￥{item.goods_price}</span><span className={ccc.s2}>￥{item.goods_marketprice}</span></p>
+							</Link>
+						</li>
+					)
+				}
+			</ul>
 		</div>
 	}
 
