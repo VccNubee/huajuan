0d81912e850db76666bd9a83785f35bc3c9deb86
liyj <1983918044@qq.com>
2019-02-27 14:02:36 +0800

    search

--- src/views/search/index.js
+++ src/views/search/index.js
@@ -1,41 +1,59 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 import styles from './index.module.scss'
-import { searchHongren, searchGoods } from "./model";
+import {searchHongren, searchGoods} from "./model";
 import store from "../../store";
 
-class Search extends Component{
-	constructor(props) {
-	  super(props);
+class Search extends Component {
+		constructor(props) {
+				super(props);
 
-	  this.state = {
-	  		goodsData: [],
-				hongrenData: []
-		};
-	}
+				this.state = {
+						searchValue: '',
+						goodsData: [],
+						hongrenData: []
+				};
+		}
 
-
-	render(){
-		return (
-				<div className={styles.search}>
-
-				</div>
-		)
-	}
-
-		componentWillMount() {
-				store.dispatch({
-						type:'isShow',
-						payLoad:false
-				})
+		render() {
+				return (
+						<div className={styles.search}>
+								<div className={styles.layer}>
+										<div className={styles.hongren}>
+												<h3 className={styles.hongrenTitle}>“{this.state.searchValue}”相关红人</h3>
+												<div className={styles.hongrenContent}></div>
+										</div>
+										<div className={styles.product}>
+												<h3 className={styles.productTitle}>“{this.state.searchValue}”相关商品</h3>
+												<div className={styles.productContent}></div>
+										</div>
+								</div>
+						</div>
+				)
 		}
 
 
 		componentDidMount() {
-			console.log(this.props.location);
-			searchGoods("面膜",0).then(res => {
-					console.log(res)
-			})
-	}
+				store.dispatch({
+						type: 'isShow',
+						payLoad: false
+				});
+				console.log(this.props.location);
+				let searchValue = this.props.location.search.slice(4);
+				// console.log();
+				this.setState({
+						searchValue: decodeURI(searchValue)
+				});
+				searchGoods(searchValue, 0).then(res => {
+						this.setState({
+								goodsData: res.goods_infos
+						})
+				});
+				searchHongren(searchValue, 0).then(res => {
+						this.setState({
+								hongrenData: res.hongren_infos
+						})
+				})
+		}
 
 }
 
--- src/views/search/model.js
+++ src/views/search/model.js
@@ -2,13 +2,13 @@ import axios from 'axios'
 
 function searchHongren(kw) {
 		return axios.get(`/search/searchHongren?KW=${kw}`).then(res => {
-				return res
+				return res.data
 		})
 }
 
 function searchGoods(kw, offset) {
 		return axios.get(`/search/searchGoods?KW=${kw}&offset=${offset}`).then(res => {
-				return res
+				return res.data
 		})
 
 }
